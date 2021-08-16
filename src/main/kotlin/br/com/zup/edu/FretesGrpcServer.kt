ca7c15625962253122c92a3667f1eed2c5bd2eea
Lucas Lacerda <lucaslacerdajob@gmail.com>
2021-08-16 15:28:13 -0300

    Retornando código de Erro

--- src/main/kotlin/br/com/zup/edu/FretesGrpcServer.kt
+++ src/main/kotlin/br/com/zup/edu/FretesGrpcServer.kt
@@ -1,6 +1,12 @@
 package br.com.zup.edu
+import com.google.protobuf.Any
+import com.google.rpc.Code
+import io.grpc.Status
+import io.grpc.StatusRuntimeException
+import io.grpc.protobuf.StatusProto
 import io.grpc.stub.StreamObserver
 import org.slf4j.LoggerFactory
+import java.lang.IllegalStateException
 import javax.inject.Singleton
 import kotlin.random.Random
 
@@ -13,9 +19,56 @@ class FretesGrpcServer : FretesServiceGrpc.FretesServiceImplBase(){
 
         logger.info("Calculando frete para request: $request")
 
+        val cep = request?.cep
+        if(cep == null || cep.isBlank()){
+            val e = Status.INVALID_ARGUMENT
+                .withDescription("cep deve ser informado")
+                .asRuntimeException()
+            responseObserver?.onError(e)
+        }
+
+        if(!cep!!.matches("[0-9]{5}-[0-9]{3}".toRegex())){
+            val e = Status.INVALID_ARGUMENT
+                .withDescription("cep invalido")
+                .augmentDescription("formato esperado deve ser 99999-999")
+                .asRuntimeException()
+            responseObserver?.onError(e)
+        }
+
+
+//        //SIMULAR uma verificação de segurança
+        if(cep!!.endsWith("333")){
+            val statusProto = com.google.rpc.Status.newBuilder()
+                .setCode(Code.PERMISSION_DENIED.number)
+                .setMessage("usuario nao pode acessar esse recurso")
+                .addDetails(Any.pack(ErrorDetails.newBuilder()
+                            .setCode(401)
+                            .setMessage("token expirado")
+                            .build()))
+                .build()
+
+            val e = StatusProto.toStatusRuntimeException(statusProto)
+            responseObserver?.onError(e)
+
+        }
+
+
+        var valor = 0.0
+        try{
+            valor = Random.nextDouble(from = 0.0, until = 4.0)
+            if(valor>3.0){
+                throw IllegalStateException("Erro inesperado na logica de negocio")
+            }
+        }catch (e: Exception){
+            responseObserver?.onError(Status.INTERNAL
+                .withDescription(e.message)
+                .withCause(e)
+                .asRuntimeException())
+        }
+
         val response = CalculaFreteResponse.newBuilder()
             .setCep(request!!.cep)
-            .setValor(Random.nextDouble(from = 0.0, until = 4.0))
+            .setValor(valor)
             .build()
 
         logger.info("Frete calculado: $response")
